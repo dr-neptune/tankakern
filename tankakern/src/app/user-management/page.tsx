f4f99511f006172201e9bc43c6de79b15b2f7bb0
dr-neptune <mrosedatasci@gmail.com>
2025-03-08 18:25:35 -0500

    Revert "fix: Change HTTP method from PUT to POST for user management update"
    This reverts commit 287a58a19ba2eb1435b529dd08c4b8e8409a8c45.

--- tankakern/src/app/user-management/page.tsx
+++ tankakern/src/app/user-management/page.tsx
@@ -45,7 +45,7 @@ export default function UserManagement() {
         localStorage.setItem("user", JSON.stringify(parsed));
         
         const res = await fetch("/api/user_management", {
-          method: "POST",
+          method: "PUT",
           headers: {
             "Content-Type": "application/json"
           },
